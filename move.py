37200f59827455f4831397ba89f7711b214fc58f
Eparrr <elozhmarsel@gmail.com>
2022-11-26 14:26:28 +0300

    1

--- move.py
+++ move.py
@@ -15,3 +15,47 @@ def controlls(v):
     else:
         v=(0,0) 
     return v
+
+#ВЫСТРЕЛЫ (вариант 1)
+
+class Bullet:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.Vx = 0
+        self.Vy = 0
+        self.dest_x = 0
+        self.dest_y = 0
+
+    def move(self):
+        self.x += self.Vx
+        if self.x <= display_width:
+            display.blit(bullet , (self.x , self. y)) #ПУЛЯ ЗДЕСЬ!
+            return True
+        else:
+            return False
+
+    def find_path(self, dest_x, dest_y):
+        self.dest_x = dest_x
+        self.dest_y = dest_y
+
+        dx = dest_x - self.x
+        count_up = dx // self.Vx
+ 
+        if self.y >= dest_y: 
+             dy = self.y - dest_y
+             self.Vy = dy / count_up
+        else:
+            dy = dest_y -self.y
+            self.Vy = - (dy / count_up)
+
+
+    def move_to(self):
+         self.x += self.Vx
+         self.y -= self.Vy
+
+        if self.x <= self.dest_x:
+            display.blit(bullet , (self.x , self.y))
+            return True
+        else:
+            return False
